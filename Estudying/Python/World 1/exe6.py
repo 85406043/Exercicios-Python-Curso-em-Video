2f70801fb9ab2ed163198508971e41d1671713db
mac <85289658+85406043@users.noreply.github.com>
2026-01-27 09:44:24 -0300

    Create exe6.py

---
+++ Estudying/Python/World 1/exe6.py
@@ -0,0 +1,8 @@
+import math
+number = int(input("Enter a number: "))
+double = number*2
+triple = number*3
+square_root = math.isqrt(number)
+print("The Double: {}".format(double))
+print("The Triple: {}".format(triple))
+print("The Square Root: {}".format(square_root))
