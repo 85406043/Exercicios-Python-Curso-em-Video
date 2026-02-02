1abe5ac688c78760c998d8fd2b607aebdf383cb4
mac <85289658+85406043@users.noreply.github.com>
2026-02-02 09:49:03 -0300

    Create exe19.py

---
+++ Estudying/Python/World 1/exe19.py
@@ -0,0 +1,8 @@
+#import module
+import random
+#list of students
+list_students = ["Beatriz", "Amaral", "Raquel", "Ronaldo"]
+#module choice
+chosen = random.choice(list_students)
+#show chosen
+print("The student chosen was: {}".format(chosen))
