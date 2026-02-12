e7b8d1ebe43599bd9557a93051d7b9aec09dd624
mac <85289658+85406043@users.noreply.github.com>
2026-02-12 11:42:06 -0300

    Create exe30.py

---
+++ Estudying/Python/World 1/exe30.py
@@ -0,0 +1,9 @@
+#i need a initial number
+number = int(input("Enter a number: "))
+#calc
+calcNumber = number % 2
+#conditions
+if calcNumber == 0:
+  print("Even number")
+else:
+  print("Odd number")
