b8b7c9303c151208a72f20e6862e177c157e4c29
mac <85289658+85406043@users.noreply.github.com>
2026-02-13 09:19:09 -0300

    Create exe34.py

---
+++ Estudying/Python/World 1/exe34.py
@@ -0,0 +1,21 @@
+#variables numbers
+firstNumber = int(input("Enter a number: "))
+secondNumber = int(input("Enter a Second number: "))
+thirdNumber = int(input(" Enter a Third number: "))
+
+#variables Larger and Smaller
+largerNumber = smallerNumber = firstNumber
+
+#conditions
+if secondNumber > firstNumber:
+  largerNumber = secondNumber
+if secondNumber < firstNumber:
+  smallerNumber = secondNumber
+
+if thirdNumber > firstNumber:
+  largerNumber = thirdNumber
+if thirdNumber < firstNumber:
+  smallerNumber = thirdNumber
+
+print(f"Larger Number: {largerNumber}")
+print(f"Smaller Number: {smallerNumber}")
