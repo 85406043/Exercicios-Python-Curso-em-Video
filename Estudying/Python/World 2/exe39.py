3c9ad02e98420f2965efa820e1b0257c6e4b9550
mac <85289658+85406043@users.noreply.github.com>
2026-02-23 14:53:36 -0300

    Update exe39.py

--- Estudying/Python/World 2/exe39.py
+++ Estudying/Python/World 2/exe39.py
@@ -5,5 +5,23 @@ actuallyYear = datetime.date.today().year
 ageActually = actuallyYear - year
 print(ageActually)
 
+#conditions
+#minor
 if ageActually < 18:
-  
+  print("Minor;")
+  calcAge = 18 - ageActually
+  yearEnlistment = year + 18
+  print(f"{calcAge} years until enlistment.")
+  print(f"Years of enlistment is {yearEnlistment}.")
+
+#quite
+elif ageActually == 18:
+  print("Enlist urgently.")
+
+#adult
+elif ageActually > 18:
+  print("Adult;")
+  lateTime = ageActually - 18
+  yearEnlistment = actuallyYear - lateTime
+  print(f"{lateTime} years have passed since your enlistment.")
+  print(f"The year of enlistment was {yearEnlistment}.")
