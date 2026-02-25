f2634809ac4567d8a374a2308b779fd443bd2fe3
mac <85289658+85406043@users.noreply.github.com>
2026-02-25 15:31:36 -0300

    Update exe42.py

--- Estudying/Python/World 2/exe42.py
+++ Estudying/Python/World 2/exe42.py
@@ -7,3 +7,13 @@
 – ESCALENO: todos os lados diferentes"""
 
 
+firstAngle = float(input("Enter a first Angle: "))
+secondAngle = float(input("Enter a second Angle: "))
+thirdAngle = float(input("Enter a third Angle: "))
+
+if firstAngle == secondAngle == thirdAngle :
+  print("Perfect Triangle! Triangle Equilateral.")
+elif firstAngle == secondAngle or secondAngle == thirdAngle or firstAngle == thirdAngle:
+  print("Triangle Isosceles.")
+else:
+  print("Triangle Scalene.")
