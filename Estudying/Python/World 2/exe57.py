4085ba3b5d19e938b4486ee2ae133c62991a3a7a
mac <85289658+85406043@users.noreply.github.com>
2026-03-10 08:14:47 -0300

    Update exe57.py

--- Estudying/Python/World 2/exe57.py
+++ Estudying/Python/World 2/exe57.py
@@ -1,14 +1,3 @@
-"""#loop
-while True:
-  #variable
-  sexUser = str(input("Your Sex: ")).upper().replace(" ", "")
-  #conditions
-  if sexUser != "F" and sexUser != "M":
-    print("Error: Type M or F.")
-    pass
-  else:
-    print(f"Success! Your gender has been successfully registered.")
-    break"""
 #variable
 condition = False
 #loop
